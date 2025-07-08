885d7c47e079610c0c5edad760afb221b89397ee
samirzjadhav <samirzjadhav@gmail.com>
2025-07-08 15:57:39 +0530

    feat: added custom style for website

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Nav from "./Components/Nav/Nav";
+import Index from "./Components/Pages/Index";
 
 const App = () => {
   return (
     <>
-      <Nav />
+      <Router>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Index />} />
+        </Routes>
+      </Router>
     </>
   );
 };
--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -1,4 +1,9 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import bookmark from "../../assets/bookmark.svg";
+
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 const Nav = () => {
   return (
