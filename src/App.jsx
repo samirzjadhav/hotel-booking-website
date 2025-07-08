814efc2fbc30484ed9d8ee72ac2840b332249d6f
samirzjadhav <samirzjadhav@gmail.com>
2025-07-08 15:37:51 +0530

    feat: added new rooms json file with infomation

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import Nav from "./Components/Nav/Nav";
 
 const App = () => {
   return (
-    <div>
-      <h1 class="text-3xl font-bold underline"> Hello world! </h1>
-    </div>
+    <>
+      <Nav />
+    </>
   );
 };
 
