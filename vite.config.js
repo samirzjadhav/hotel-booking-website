55ddc260ac66ec493f543636e46115e90e22dd9e
samirzjadhav <samirzjadhav@gmail.com>
2025-09-23 20:46:47 +0530

    fix: vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import vercel from "vite-plugin-vercel";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), vercel()],
 });
